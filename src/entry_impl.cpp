9e862b7e7e4be8e763d0b8338f779834965d6fd7
Dmitry Bondin <dbondin@ya.ru>
2013-04-09 10:56:00 +0400

    add index to session identity

--- src/entry_impl.cpp
+++ src/entry_impl.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
+#include <string>
 #include <Ice/Ice.h>
 
 #include "labrestapi.h"
 #include "entry_impl.h"
 #include "session_impl.h"
 
+entry_impl::entry_impl() {
+    index = 0;
+}
+
 ::labrestapi::sessionPrx
 entry_impl::login(const ::std::string& username, const ::std::string& auth, const ::Ice::Current& current) {
+
+    ::std::string sessionId = "SimpleSession" + index++;
 
     ::std::cout<<"entry_impl::login() called"<<::std::endl;
 
     Ice::ObjectPtr object = new session_impl;
 
-    ::labrestapi::sessionPrx sessionProxy = ::labrestapi::sessionPrx::checkedCast(current.adapter->add(object, current.adapter->getCommunicator()->stringToIdentity("SimpleSession")));
+    ::labrestapi::sessionPrx sessionProxy = ::labrestapi::sessionPrx::checkedCast(current.adapter->add(object, current.adapter->getCommunicator()->stringToIdentity(sessionId)));
 
     return sessionProxy;
 }
--- src/entry_impl.h
+++ src/entry_impl.h
@@ -7,7 +7,13 @@ class entry_impl : public ::labrestapi::entry
 {
 public:
 
+    entry_impl();
+
     virtual ::labrestapi::sessionPrx login(const ::std::string&, const ::std::string&, const ::Ice::Current&);
+
+private:
+
+    int index;
 };
 
 #endif /* __ENTRY_IMPL_H__ */
