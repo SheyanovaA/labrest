f2a0113a2fe8f3e426f86431fc9e2aa604b91548
SheyanovaA <lina1a@bk.ru>
2013-07-15 17:54:58 +0400

    python client

--- src/LabrestClient.py
+++ src/LabrestClient.py
@@ -1,13 +1,18 @@
 import web
+import json
 import sys, traceback, Ice
 import LabrestAPI
+import re
 
 from web import form
+
 
 urls = (
 	'/', 'index',
 	'/lock/(.*)', 'lock_res',
-	'/unlock/(.*)', 'unlock_res'
+	'/unlock/(.*)', 'unlock_res',
+	'/login', 'login',
+	'/tree', 'tree'
 )
 
 web.config.debug = False
@@ -15,8 +20,8 @@ app = web.application(urls,globals())
 render  = web.template.render('Web_templates')
 
 myform = form.Form(
-    form.Textbox('username'),
-    form.Textbox('authdata')
+    form.Textbox('username',  value='Login', id="username", size="22", onfocus="if(this.value=='Login') this.value='';", onblur="if(this.value=='') this.value='Login'"),
+    form.Password('authdata', value="Passwd", id='authdata', size ="22", onfocus="if(this.value=='Passwd') this.value='';", onblur="if(this.value=='') this.value='Passwd'")
 )
 
 
@@ -44,7 +49,8 @@ def ressToTree(ress):
 		
 def getIceSession(self):
     print 'getIceSession() called'
-    icl = session_icl_map.get(self.session_id, None)
+    ice_proxy_id = self.session_id + self.username
+    icl = session_icl_map.get(ice_proxy_id, None)
     if not icl:
         print 'create new Ice session'
 	base  = ic.stringToProxy("SimpleEntry:tcp -p 10000")
@@ -52,14 +58,14 @@ def getIceSession(self):
 	if not entry:
 	    raise RuntimeError('Invalid proxy')
 	icl = entry.login(self.username,self.authdata)
-	session_icl_map[self.session_id] = icl
+	session_icl_map[ice_proxy_id] = icl
     return icl	
 
-username = 'guest'
-authdata = 'guest'
+
+
 web.session.Session.getIceSession = getIceSession
-web.session.Session.username = username
-web.session.Session.authdata = authdata
+web.session.Session.username = 'guest'
+web.session.Session.authdata = 'guest'
 web.session.Session.__icl = None
 
 session = web.session.Session(app,  web.session.DiskStore('sessions'))
@@ -68,6 +74,34 @@ class index:
     def GET(self):
 	ress = ressToTree(session.getIceSession().getResourceManager().getAllResources())
 	return render.guest_page(ress,myform)
+
+class login:
+    def GET(self):
+	form = myform()
+        form.validates()
+	print form.value['username']
+	session.username = form.value['username']
+	session.authdata = form.value['authdata']
+	print session.username
+	s = session.getIceSession().getUserManager().getUser('admin')
+	w = json.dumps(s.__dict__)
+	print w
+	return w
+
+class tree:
+    def res2dict(self, res):
+	result = {};
+	result = res.__dict__
+	result['resLockStatus'] = res.resLockStatus.__dict__
+	return result
+
+    def GET(self):
+	ress = session.getIceSession().getResourceManager().getAllResources()
+	res_list = []
+	for res in ress:
+            res_list.append(self.res2dict(res))
+	w = json.dumps(res_list)
+	return w
 
 class lock_res:
     def GET(self,res_id):
